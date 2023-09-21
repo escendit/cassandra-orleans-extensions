117d30fcd0ff57321d0e6503cf9cd0d91613ef34
Simon Novak <snovak7@gmail.com>
2023-09-21 22:45:44 +0200

    chore: Added TestState.cs

---
+++ test/Persistence/Cassandra/Grains/TestState.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Tests.Grains;
+
+/// <summary>
+/// Test Stage.
+/// </summary>
+[GenerateSerializer]
+public class TestState
+{
+    /// <summary>
+    /// Gets or sets the state.
+    /// </summary>
+    /// <value>The state.</value>
+    [Id(0)]
+    public string? State { get; set; }
+}
