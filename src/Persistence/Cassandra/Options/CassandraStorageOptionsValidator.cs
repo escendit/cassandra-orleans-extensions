57304e3c587c1f9b8e07bf4f05f0696d8b00edcc
Simon Novak <snovak7@gmail.com>
2023-09-21 22:32:17 +0200

    feature: Added CassandraStorageOptionsValidator.cs

---
+++ src/Persistence/Cassandra/Options/CassandraStorageOptionsValidator.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Options;
+
+using global::Orleans;
+
+/// <summary>
+/// Cassandra Storage Options Validator.
+/// </summary>
+public class CassandraStorageOptionsValidator : IConfigurationValidator
+{
+    private readonly CassandraStorageOptions _options;
+    private readonly string _name;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraStorageOptionsValidator"/> class.
+    /// </summary>
+    /// <param name="options">The options.</param>
+    /// <param name="name">The name.</param>
+    public CassandraStorageOptionsValidator(CassandraStorageOptions options, string name)
+    {
+        _options = options;
+        _name = name;
+    }
+
+    /// <inheritdoc/>
+    public void ValidateConfiguration()
+    {
+    }
+}
