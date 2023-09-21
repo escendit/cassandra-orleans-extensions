df1f55b4aeaa9041d654ca41348e9f65b36b8e44
Simon Novak <snovak7@gmail.com>
2023-09-21 22:31:56 +0200

    feature: Added CassandraStorageException.cs

---
+++ src/Persistence/Cassandra/Storage/CassandraStorageException.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Storage;
+
+using System.Runtime.Serialization;
+using global::Orleans;
+
+/// <summary>
+/// Cassandra Storage Exception.
+/// </summary>
+[GenerateSerializer]
+public class CassandraStorageException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraStorageException"/> class.
+    /// </summary>
+    public CassandraStorageException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraStorageException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public CassandraStorageException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraStorageException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public CassandraStorageException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraStorageException"/> class.
+    /// </summary>
+    /// <param name="serializationInfo">The serialization info.</param>
+    /// <param name="streamingContext">The streaming context.</param>
+    protected CassandraStorageException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+        : base(serializationInfo, streamingContext)
+    {
+    }
+}
