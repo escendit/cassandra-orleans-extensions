e24ea31075573e92832c83239f627bc0914fb3a1
Simon Novak <snovak7@gmail.com>
2023-09-21 22:31:22 +0200

    feature: Added CassandraGrainStorageFactory.cs

---
+++ src/Persistence/Cassandra/Storage/CassandraGrainStorageFactory.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Storage;
+
+using Escendit.Extensions.Hosting.Cassandra;
+using global::Orleans;
+using Microsoft.Extensions.DependencyInjection;
+using Options;
+
+/// <summary>
+/// Cassandra Grain Storage Factory.
+/// </summary>
+public static class CassandraGrainStorageFactory
+{
+    /// <summary>
+    /// Create GrainStorage.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider.</param>
+    /// <param name="name">The name.</param>
+    /// <returns>The grain storage.</returns>
+    public static GrainStorageBase Create(IServiceProvider serviceProvider, string name)
+    {
+        var options = serviceProvider.GetOptionsByName<CassandraStorageOptions>(name);
+        var connectionOptions = serviceProvider.GetOptionsByName<CassandraClientOptions>(name);
+        return options.Strategy switch
+        {
+            Strategy.SingleTable => ActivatorUtilities
+                .CreateInstance<SingleTableGrainStorage>(serviceProvider, name, options, connectionOptions),
+            Strategy.TablePerGrain => throw new NotImplementedException(),
+            _ => throw new ArgumentOutOfRangeException(name),
+        };
+    }
+}
