2b804775465840938981d0b0a106727076a396b9
Simon Novak <snovak7@gmail.com>
2023-09-21 22:39:13 +0200

    chore: Added SiloBuilderFixture.cs

---
+++ test/Persistence/Cassandra/Fixtures/SiloBuilderFixture.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Tests.Fixtures;
+
+/// <summary>
+/// Silo Builder Fixture.
+/// </summary>
+public sealed class SiloBuilderFixture
+{
+}
