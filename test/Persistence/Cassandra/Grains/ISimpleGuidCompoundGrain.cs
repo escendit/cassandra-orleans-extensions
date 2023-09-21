485270c7154d167ffcbe3727988eb1d4be1b501c
Simon Novak <snovak7@gmail.com>
2023-09-21 22:37:05 +0200

    chore: Added ISimpleGuidCompoundGrain.cs

---
+++ test/Persistence/Cassandra/Grains/ISimpleGuidCompoundGrain.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Tests.Grains;
+
+/// <summary>
+/// Simple Guid Compound Grain Interface.
+/// </summary>
+public interface ISimpleGuidCompoundGrain : IGrainWithGuidCompoundKey, ITestGrain
+{
+}
