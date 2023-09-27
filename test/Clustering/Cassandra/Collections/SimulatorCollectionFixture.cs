3f7cf0b3572f86570d24929deda5bfe931ec991f
Simon Novak <snovak7@gmail.com>
2023-09-27 21:43:59 +0200

    chore: Added SimulatorCollectionFixture.cs

---
+++ test/Clustering/Cassandra/Collections/SimulatorCollectionFixture.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clustering.Cassandra.Tests.Collections;
+
+using Fixtures;
+using Xunit;
+
+/// <summary>
+/// Simulator Collection Fixture.
+/// </summary>
+[CollectionDefinition(Name)]
+public class SimulatorCollectionFixture : ICollectionFixture<SimulatorFixture>
+{
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public const string Name = "SimulatorCollection";
+}
