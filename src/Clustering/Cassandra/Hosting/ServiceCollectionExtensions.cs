8c4a09e96bdaef189a4c179e48cc5e74fd0b1a9d
Simon Novak <snovak7@gmail.com>
2023-09-27 21:43:05 +0200

    feature: Added ServiceCollectionExtensions.cs

---
+++ src/Clustering/Cassandra/Hosting/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+using Escendit.Orleans.Clustering.Cassandra;
+using Escendit.Orleans.Clustering.Cassandra.Options;
+using Options;
+using Orleans;
+
+/// <summary>
+/// Service Collection Extensions.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add Cassandra Clustering.
+    /// </summary>
+    /// <param name="services">The initial service collection.</param>
+    /// <param name="configureOptions">The configure options.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddCassandraClustering(
+        this IServiceCollection services,
+        Action<CassandraClusteringOptions> configureOptions)
+    {
+        return services
+            .AddCassandraClustering(builder => builder.Configure(configureOptions));
+    }
+
+    /// <summary>
+    /// Add Cassandra Clustering.
+    /// </summary>
+    /// <param name="services">The initial service collection.</param>
+    /// <param name="configureOptions">The configure options.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddCassandraClustering(
+        this IServiceCollection services,
+        Action<OptionsBuilder<CassandraClusteringOptions>> configureOptions)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configureOptions);
+        configureOptions.Invoke(services.AddOptions<CassandraClusteringOptions>());
+        return services
+            .AddSingleton<IMembershipTable>(serviceProvider =>
+                new CassandraMembershipTable(
+                    serviceProvider, serviceProvider.GetRequiredService<IOptions<CassandraClusteringOptions>>().Value));
+    }
+}
