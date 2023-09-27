4c463a7010aaa9277cd16e55e67769a811e2215e
Simon Novak <snovak7@gmail.com>
2023-09-27 21:38:46 +0200

    feature: Added CassandraGatewayListProvider.cs

---
+++ src/Clustering/Cassandra/Provider/CassandraGatewayListProvider.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clustering.Cassandra;
+
+using global::Orleans.Messaging;
+
+/// <summary>
+/// Cassandra Gateway List Provider.
+/// </summary>
+public class CassandraGatewayListProvider : IGatewayListProvider
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CassandraGatewayListProvider"/> class.
+    /// </summary>
+    public CassandraGatewayListProvider()
+    {
+    }
+
+    /// <inheritdoc/>
+    public TimeSpan MaxStaleness { get; } = TimeSpan.Zero;
+
+    /// <inheritdoc/>
+    public bool IsUpdatable { get; } = true;
+
+    /// <inheritdoc/>
+    public Task InitializeGatewayListProvider()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public Task<IList<Uri>> GetGateways()
+    {
+        throw new NotImplementedException();
+    }
+}
