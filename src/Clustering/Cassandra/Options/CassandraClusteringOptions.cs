d58cd80e5cb3c4c7f6c22e54aef5a22aaed24e29
Simon Novak <snovak7@gmail.com>
2023-09-27 21:38:10 +0200

    feature: Added CassandraClusteringOptions.cs

---
+++ src/Clustering/Cassandra/Options/CassandraClusteringOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clustering.Cassandra.Options;
+
+/// <summary>
+/// Cassandra Clustering Options.
+/// </summary>
+public record CassandraClusteringOptions
+{
+    /// <summary>
+    /// Gets or sets the client name.
+    /// </summary>
+    /// <value>The client name.</value>
+    public string? ClientName { get; set; }
+}
