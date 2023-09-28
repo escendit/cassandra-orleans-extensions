73379e5b260d96fae208b258735cc513d7733767
Simon Novak <snovak7@gmail.com>
2023-09-28 22:38:55 +0200

    feature: Added ClientBuilderExtensions.cs

---
+++ src/Clustering/Cassandra/Hosting/ClientBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Orleans.Hosting;
+
+using Escendit.Orleans.Clustering.Cassandra;
+using Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Client Builder Extensions.
+/// </summary>
+public static class ClientBuilderExtensions
+{
+    /// <summary>
+    /// Use Cassandra Clustering.
+    /// </summary>
+    /// <param name="clientBuilder">The initial silo builder.</param>
+    /// <returns>The updated silo builder.</returns>
+    public static IClusteringClientBuilder UseCassandraClustering(this IClientBuilder clientBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(clientBuilder);
+        return new ClusteringClientBuilder(clientBuilder
+            .Services
+            .AddSingleton<IGatewayListProvider, CassandraGatewayListProvider>());
+    }
+}
