7c13daea8c45fad1cea55496f63af9c5eb87d397
Simon Novak <snovak7@gmail.com>
2023-09-28 22:39:30 +0200

    chore: Added ClusterCollectionFixture.cs

---
+++ test/Clustering/Cassandra/Collections/ClusterCollectionFixture.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clustering.Cassandra.Tests.Collections;
+
+using Escendit.Orleans.Persistence.Cassandra.Tests.Fixtures;
+using Xunit;
+
+/// <summary>
+/// Cluster Collection Fixture.
+/// </summary>
+[CollectionDefinition(Name)]
+public class ClusterCollectionFixture : ICollectionFixture<TestClusterFixture>
+{
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public const string Name = "ClusterCollection";
+}
