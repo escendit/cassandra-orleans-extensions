08fe23d5ae7cdabb7d0082c6dadd44d85fcd8bbd
Simon Novak <snovak7@gmail.com>
2023-09-28 22:39:45 +0200

    feature: Added ClusteringClientBuilder.cs

---
+++ src/Clustering/Cassandra/Hosting/ClusteringClientBuilder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Orleans.Hosting;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Clustering Client Builder.
+/// </summary>
+internal class ClusteringClientBuilder : IClusteringClientBuilder
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClusteringClientBuilder"/> class.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection.</param>
+    public ClusteringClientBuilder(IServiceCollection serviceCollection)
+    {
+        Services = serviceCollection;
+    }
+
+    /// <inheritdoc/>
+    public IServiceCollection Services { get; }
+}
