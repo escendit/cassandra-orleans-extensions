052ac3b466e21b64157df0219c936a854db8b718
Simon Novak <snovak7@gmail.com>
2023-09-28 23:49:34 +0200

    chore: Added TestClientBuilderConfigurator.cs

---
+++ test/Clustering/Cassandra/Configuration/TestClientBuilderConfigurator.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Tests.Configuration;
+
+using global::Orleans.TestingHost;
+using Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Test Client Builder Configurator.
+/// </summary>
+public class TestClientBuilderConfigurator : IClientBuilderConfigurator
+{
+    /// <inheritdoc/>
+    public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+    {
+        clientBuilder
+            .UseCassandraClustering()
+            .WithClientAsDefault(options =>
+            {
+                options.Endpoints.Add("localhost");
+                options.DefaultKeyspace = "test";
+            })
+            .WithOptions(options => options.MaxStaleness = TimeSpan.FromDays(1));
+    }
+}
