ad3cab0a55be1de9849cb127640f99c9d7872fd0
Simon Novak <snovak7@gmail.com>
2023-09-28 22:44:49 +0200

    chore: Added TestSiloConfigurator.cs

---
+++ test/Clustering/Cassandra/Configuration/TestSiloConfigurator.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Persistence.Cassandra.Tests.Configuration;
+
+using global::Orleans.TestingHost;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Test Silo Configurator.
+/// </summary>
+public class TestSiloConfigurator : ISiloConfigurator
+{
+    /// <inheritdoc/>
+    public void Configure(ISiloBuilder siloBuilder)
+    {
+        siloBuilder
+            .ConfigureLogging(options =>
+            {
+                options.AddConsole();
+                options.SetMinimumLevel(LogLevel.Debug);
+            })
+            .UseCassandraClustering()
+            .WithClientAsDefault(options =>
+            {
+                options.Endpoints.Add("localhost");
+                options.DefaultKeyspace = "test";
+            })
+            .WithOptions(options => options.MaxStaleness = TimeSpan.FromDays(1));
+    }
+}
