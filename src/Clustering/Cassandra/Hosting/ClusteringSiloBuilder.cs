b141177825016ee25b5350de6cd43831bee91876
Simon Novak <snovak7@gmail.com>
2023-09-28 22:40:23 +0200

    feature: Added ClusteringSiloBuilder.cs

---
+++ src/Clustering/Cassandra/Hosting/ClusteringSiloBuilder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Orleans.Hosting;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Clustering Silo Builder.
+/// </summary>
+internal class ClusteringSiloBuilder : IClusteringSiloBuilder
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ClusteringSiloBuilder"/> class.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection.</param>
+    public ClusteringSiloBuilder(IServiceCollection serviceCollection)
+    {
+        Services = serviceCollection;
+    }
+
+    /// <inheritdoc/>
+    public IServiceCollection Services { get; }
+}
