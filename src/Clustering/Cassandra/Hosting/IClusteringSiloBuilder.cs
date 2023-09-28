2c513f5d6135f0b0a103ac605125c2b3922cfd6e
Simon Novak <snovak7@gmail.com>
2023-09-28 22:43:02 +0200

    feature: Added IClusteringSiloBuilder.cs

---
+++ src/Clustering/Cassandra/Hosting/IClusteringSiloBuilder.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Orleans.Hosting;
+
+/// <summary>
+/// Clustering Silo Builder.
+/// </summary>
+public interface IClusteringSiloBuilder : ISiloBuilder
+{
+}
