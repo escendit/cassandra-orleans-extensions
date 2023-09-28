2b73826ff2eb849ae15f563ccde1de0fbc5cc4dc
Simon Novak <snovak7@gmail.com>
2023-09-28 22:40:54 +0200

    feature: Added DataProvider.cs

---
+++ src/Clustering/Cassandra/Provider/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clustering.Cassandra;
+
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Data Provider.
+/// </summary>
+public partial class DataProvider
+{
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataProvider"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    public DataProvider(
+        ILogger<DataProvider> logger)
+    {
+        _logger = logger;
+    }
+
+    [LoggerMessage(
+        EventId = 100,
+        EventName = "",
+        Level = LogLevel.Information,
+        Message = "Fine")]
+    private partial void LogSomething();
+}
