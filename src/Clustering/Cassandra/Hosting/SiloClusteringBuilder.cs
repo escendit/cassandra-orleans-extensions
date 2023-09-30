c3686e0b82722d95475df64fe631e70e4c8417e3
Simon Novak <snovak7@gmail.com>
2023-09-30 17:13:31 +0200

    feature: Updated SiloClusteringBuilder.cs

--- src/Clustering/Cassandra/Hosting/SiloClusteringBuilder.cs
+++ src/Clustering/Cassandra/Hosting/SiloClusteringBuilder.cs
@@ -3,22 +3,26 @@
 
 namespace Orleans.Hosting;
 
+using Escendit.Extensions.Hosting.Cassandra;
 using Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
 /// Clustering Silo Builder.
 /// </summary>
-internal class ClusteringSiloBuilder : IClusteringSiloBuilder
+internal class SiloClusteringBuilder : ISiloClusteringBuilder
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="ClusteringSiloBuilder"/> class.
+    /// Initializes a new instance of the <see cref="SiloClusteringBuilder"/> class.
     /// </summary>
     /// <param name="serviceCollection">The service collection.</param>
-    public ClusteringSiloBuilder(IServiceCollection serviceCollection)
+    public SiloClusteringBuilder(IServiceCollection serviceCollection)
     {
         Services = serviceCollection;
     }
 
     /// <inheritdoc/>
     public IServiceCollection Services { get; }
+
+    /// <inheritdoc/>
+    public string Name { get; set; } = CassandraClientOptions.DefaultOptionsKey;
 }
