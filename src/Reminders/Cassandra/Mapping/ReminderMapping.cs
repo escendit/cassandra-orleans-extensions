e2b257f3f7dcdbebdf42ce59950f826cb8fb67a1
Simon Novak <snovak7@gmail.com>
2023-09-30 23:47:52 +0200

    feature: Added ReminderMapping.cs

---
+++ src/Reminders/Cassandra/Mapping/ReminderMapping.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Reminders.Cassandra.Mapping;
+
+using global::Cassandra.Mapping;
+using Schema;
+
+/// <summary>
+/// Reminder Mapping.
+/// </summary>
+public class ReminderMapping : Mappings
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReminderMapping"/> class.
+    /// </summary>
+    public ReminderMapping()
+    {
+        For<Reminder>()
+            .TableName("reminders")
+            .ClusteringKey("type", "id", "name")
+            .Column(p => p.Type, cm => cm.WithName("type"))
+            .Column(p => p.Id, cm => cm.WithName("id"))
+            .Column(p => p.Hash, cm => cm.WithName("hash"))
+            .Column(p => p.Name, cm => cm.WithName("name"))
+            .Column(p => p.StartOn, cm => cm.WithName("start_on"))
+            .Column(p => p.Period, cm => cm.WithName("period"))
+            .Column(p => p.Etag, cm => cm.WithName("etag"));
+    }
+}
