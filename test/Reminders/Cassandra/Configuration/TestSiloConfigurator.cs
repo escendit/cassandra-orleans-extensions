4c83295a44346e29835e00656cf05d28a00c08bd
Simon Novak <snovak7@gmail.com>
2023-10-01 14:03:58 +0200

    chore: Added TestSiloConfigurator.cs

---
+++ test/Reminders/Cassandra/Configuration/TestSiloConfigurator.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Reminders.Cassandra.Tests.Configuration;
+
+using global::Orleans.TestingHost;
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// Test Silo Configurator.
+/// </summary>
+public class TestSiloConfigurator : ISiloConfigurator
+{
+    /// <inheritdoc/>
+    public void Configure(ISiloBuilder siloBuilder)
+    {
+        siloBuilder
+            .ConfigureLogging(options =>
+            {
+                options.AddConsole();
+                options.AddFilter("Orleans", LogLevel.Information);
+                options.AddFilter("Escendit", LogLevel.Trace);
+                options.SetMinimumLevel(LogLevel.Debug);
+            })
+            .AddMemoryGrainStorageAsDefault()
+            .UseCassandraReminderService(options =>
+            {
+                options
+                    .Endpoints
+                    .Add("localhost");
+                options.DefaultKeyspace = "test";
+            });
+    }
+}
